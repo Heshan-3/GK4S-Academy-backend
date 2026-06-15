e901c571dc3a1969812aa40971859d2152355358
Heshan Kawithra <heshankawithra2021@gmail.com>
2026-06-15 14:37:10 +0530

    dotenv fix

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ import requestRouter from "./Routers/requestRoute.js";
 import { fileURLToPath } from "url";
 
 // Load environment variables based on NODE_ENV
-const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development";
-dotenv.config({ path: envFile });
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
